2641a65b2e2b6d4ab0fc824aa59a04c5a75392a4
arunvarma3344 <arunvarma96.p@gmail.com>
2020-07-23 12:09:43 +0000

    Lab 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Update this constant with your ServiceNow credentials
 const options = {
-  url: ''https://dev68181.service-now.com,
+  url: 'https://dev68181.service-now.com',
   username: 'admin',
   password: 'Lockdown@123'
 };
